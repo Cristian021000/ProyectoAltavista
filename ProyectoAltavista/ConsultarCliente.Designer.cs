ea32012f6643e2a7eaa6588b8b761b84a00c5aa8
Cristian Suclupe Llontop <cristian2.10@hotmail.com>
2021-05-30 18:06:18 -0500

    Se agrego ventana consultarCliente

---
+++ ProyectoAltavista/ConsultarCliente.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace ProyectoAltavista
+{
+    partial class ConsultarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbIngreseDni = new System.Windows.Forms.Label();
+            this.txtIngreseDni = new System.Windows.Forms.TextBox();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.btRegresar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(476, 391);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lbIngreseDni
+            // 
+            this.lbIngreseDni.AutoSize = true;
+            this.lbIngreseDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIngreseDni.Location = new System.Drawing.Point(567, 47);
+            this.lbIngreseDni.Name = "lbIngreseDni";
+            this.lbIngreseDni.Size = new System.Drawing.Size(188, 31);
+            this.lbIngreseDni.TabIndex = 1;
+            this.lbIngreseDni.Text = "Ingrese el DNI";
+            // 
+            // txtIngreseDni
+            // 
+            this.txtIngreseDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIngreseDni.Location = new System.Drawing.Point(547, 101);
+            this.txtIngreseDni.Name = "txtIngreseDni";
+            this.txtIngreseDni.Size = new System.Drawing.Size(224, 31);
+            this.txtIngreseDni.TabIndex = 2;
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAceptar.Location = new System.Drawing.Point(595, 163);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(136, 60);
+            this.btAceptar.TabIndex = 3;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btRegresar
+            // 
+            this.btRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRegresar.Location = new System.Drawing.Point(624, 371);
+            this.btRegresar.Name = "btRegresar";
+            this.btRegresar.Size = new System.Drawing.Size(147, 48);
+            this.btRegresar.TabIndex = 4;
+            this.btRegresar.Text = "Regresar";
+            this.btRegresar.UseVisualStyleBackColor = true;
+            // 
+            // ConsultarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btRegresar);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.txtIngreseDni);
+            this.Controls.Add(this.lbIngreseDni);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ConsultarCliente";
+            this.Text = "ConsultarCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbIngreseDni;
+        private System.Windows.Forms.TextBox txtIngreseDni;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.Button btRegresar;
+    }
+}
