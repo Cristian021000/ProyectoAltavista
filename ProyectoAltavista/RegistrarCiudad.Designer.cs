a0ff3601a0481e249344c4cf34505e4d719823e8
KillerWolx3 <ariel2mosluvic@gmail.com>
2021-06-01 20:40:22 -0500

    Registro de ciudad

---
+++ ProyectoAltavista/RegistrarCiudad.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace ProyectoAltavista
+{
+    partial class RegistrarCiudad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistrarCiudad));
+            this.lbRegistrarCiudad = new System.Windows.Forms.Label();
+            this.lbNombreCiudad = new System.Windows.Forms.Label();
+            this.lbDireccionCiudad = new System.Windows.Forms.Label();
+            this.lbCodigoPostal = new System.Windows.Forms.Label();
+            this.lbReferencia = new System.Windows.Forms.Label();
+            this.txtNombreCiudad = new System.Windows.Forms.TextBox();
+            this.textDireccionCiudad = new System.Windows.Forms.TextBox();
+            this.textCódigoPostal = new System.Windows.Forms.TextBox();
+            this.textReferenciasCiudad = new System.Windows.Forms.TextBox();
+            this.btRegistrarCiudad = new System.Windows.Forms.Button();
+            this.btCancelarCiudad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbRegistrarCiudad
+            // 
+            this.lbRegistrarCiudad.AutoSize = true;
+            this.lbRegistrarCiudad.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRegistrarCiudad.Location = new System.Drawing.Point(257, 33);
+            this.lbRegistrarCiudad.Name = "lbRegistrarCiudad";
+            this.lbRegistrarCiudad.Size = new System.Drawing.Size(277, 39);
+            this.lbRegistrarCiudad.TabIndex = 1;
+            this.lbRegistrarCiudad.Text = "Registrar Ciudad";
+            this.lbRegistrarCiudad.Click += new System.EventHandler(this.lbRegistrarCiudad_Click);
+            // 
+            // lbNombreCiudad
+            // 
+            this.lbNombreCiudad.AutoSize = true;
+            this.lbNombreCiudad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombreCiudad.Location = new System.Drawing.Point(48, 165);
+            this.lbNombreCiudad.Name = "lbNombreCiudad";
+            this.lbNombreCiudad.Size = new System.Drawing.Size(176, 24);
+            this.lbNombreCiudad.TabIndex = 3;
+            this.lbNombreCiudad.Text = "Nombre de Ciudad:";
+            // 
+            // lbDireccionCiudad
+            // 
+            this.lbDireccionCiudad.AutoSize = true;
+            this.lbDireccionCiudad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDireccionCiudad.Location = new System.Drawing.Point(48, 215);
+            this.lbDireccionCiudad.Name = "lbDireccionCiudad";
+            this.lbDireccionCiudad.Size = new System.Drawing.Size(95, 24);
+            this.lbDireccionCiudad.TabIndex = 4;
+            this.lbDireccionCiudad.Text = "Dirección:";
+            // 
+            // lbCodigoPostal
+            // 
+            this.lbCodigoPostal.AutoSize = true;
+            this.lbCodigoPostal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCodigoPostal.Location = new System.Drawing.Point(48, 263);
+            this.lbCodigoPostal.Name = "lbCodigoPostal";
+            this.lbCodigoPostal.Size = new System.Drawing.Size(131, 24);
+            this.lbCodigoPostal.TabIndex = 5;
+            this.lbCodigoPostal.Text = "Código Postal:";
+            // 
+            // lbReferencia
+            // 
+            this.lbReferencia.AutoSize = true;
+            this.lbReferencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbReferencia.Location = new System.Drawing.Point(48, 315);
+            this.lbReferencia.Name = "lbReferencia";
+            this.lbReferencia.Size = new System.Drawing.Size(115, 24);
+            this.lbReferencia.TabIndex = 6;
+            this.lbReferencia.Text = "Referencias:";
+            // 
+            // txtNombreCiudad
+            // 
+            this.txtNombreCiudad.Location = new System.Drawing.Point(264, 170);
+            this.txtNombreCiudad.Name = "txtNombreCiudad";
+            this.txtNombreCiudad.Size = new System.Drawing.Size(360, 20);
+            this.txtNombreCiudad.TabIndex = 7;
+            // 
+            // textDireccionCiudad
+            // 
+            this.textDireccionCiudad.Location = new System.Drawing.Point(264, 219);
+            this.textDireccionCiudad.Name = "textDireccionCiudad";
+            this.textDireccionCiudad.Size = new System.Drawing.Size(360, 20);
+            this.textDireccionCiudad.TabIndex = 8;
+            // 
+            // textCódigoPostal
+            // 
+            this.textCódigoPostal.Location = new System.Drawing.Point(264, 263);
+            this.textCódigoPostal.Name = "textCódigoPostal";
+            this.textCódigoPostal.Size = new System.Drawing.Size(360, 20);
+            this.textCódigoPostal.TabIndex = 9;
+            // 
+            // textReferenciasCiudad
+            // 
+            this.textReferenciasCiudad.Location = new System.Drawing.Point(264, 315);
+            this.textReferenciasCiudad.Name = "textReferenciasCiudad";
+            this.textReferenciasCiudad.Size = new System.Drawing.Size(360, 20);
+            this.textReferenciasCiudad.TabIndex = 10;
+            // 
+            // btRegistrarCiudad
+            // 
+            this.btRegistrarCiudad.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRegistrarCiudad.Location = new System.Drawing.Point(650, 116);
+            this.btRegistrarCiudad.Name = "btRegistrarCiudad";
+            this.btRegistrarCiudad.Size = new System.Drawing.Size(138, 73);
+            this.btRegistrarCiudad.TabIndex = 12;
+            this.btRegistrarCiudad.Text = "Registrar";
+            this.btRegistrarCiudad.UseVisualStyleBackColor = true;
+            // 
+            // btCancelarCiudad
+            // 
+            this.btCancelarCiudad.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancelarCiudad.Location = new System.Drawing.Point(650, 315);
+            this.btCancelarCiudad.Name = "btCancelarCiudad";
+            this.btCancelarCiudad.Size = new System.Drawing.Size(138, 73);
+            this.btCancelarCiudad.TabIndex = 13;
+            this.btCancelarCiudad.Text = "Cancelar";
+            this.btCancelarCiudad.UseVisualStyleBackColor = true;
+            // 
+            // RegistrarCiudad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCancelarCiudad);
+            this.Controls.Add(this.btRegistrarCiudad);
+            this.Controls.Add(this.textReferenciasCiudad);
+            this.Controls.Add(this.textCódigoPostal);
+            this.Controls.Add(this.textDireccionCiudad);
+            this.Controls.Add(this.txtNombreCiudad);
+            this.Controls.Add(this.lbReferencia);
+            this.Controls.Add(this.lbCodigoPostal);
+            this.Controls.Add(this.lbDireccionCiudad);
+            this.Controls.Add(this.lbNombreCiudad);
+            this.Controls.Add(this.lbRegistrarCiudad);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RegistrarCiudad";
+            this.Text = "RegistrarCiudad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbRegistrarCiudad;
+        private System.Windows.Forms.Label lbNombreCiudad;
+        private System.Windows.Forms.Label lbDireccionCiudad;
+        private System.Windows.Forms.Label lbCodigoPostal;
+        private System.Windows.Forms.Label lbReferencia;
+        private System.Windows.Forms.TextBox txtNombreCiudad;
+        private System.Windows.Forms.TextBox textDireccionCiudad;
+        private System.Windows.Forms.TextBox textCódigoPostal;
+        private System.Windows.Forms.TextBox textReferenciasCiudad;
+        private System.Windows.Forms.Button btRegistrarCiudad;
+        private System.Windows.Forms.Button btCancelarCiudad;
+    }
+}
