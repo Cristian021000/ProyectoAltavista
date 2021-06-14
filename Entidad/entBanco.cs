8b971d50592905729f605cc3826ee2d34ea89fa2
KillerWolx3 <ariel2mosluvic@gmail.com>
2021-06-14 14:51:25 -0500

    entBanco hecho

---
+++ Entidad/entBanco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entBanco
+    {
+        public string Nombre { get; set; }
+        public int idbanco { get; set; }
+
+    }
+}
