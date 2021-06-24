c29147344764ea3a0588fc92b95ddc487094ffd7
dracz-m <darckrons@gmail.com>
2021-06-23 20:01:57 -0500

    capa datos cita

---
+++ CapaDatos/datCita.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class datCita
+    {
+        #region singleton
+        private static datCita _instancia = new datCita();
+        public static datCita Instancia
+        {
+            get { return datCita._instancia; }
+        }
+        #endregion singleton
+    }
+}
