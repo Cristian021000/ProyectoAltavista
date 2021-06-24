cd97567d495438a4884b1fa7c1214660e6bdaa5e
Thalia Capa Medina <thaliacapm@gmail.com>
2021-06-24 12:34:22 -0500

    Se agrego logCita y region singleton

---
+++ CapaLogica/logCita.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+using System.Data;
+
+namespace CapaLogica
+{
+    public class logCita
+    {
+        #region singleton
+        private static readonly logCita _instancia = new logCita();
+        public static logCita Instancia
+        {
+            get { return logCita._instancia; }
+        }
+        #endregion singleton
+
+    }
+}
