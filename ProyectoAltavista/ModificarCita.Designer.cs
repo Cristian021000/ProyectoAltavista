23e83f867f83d298d25f6136944ef9c050a1ac06
Thalia Capa Medina <thaliacapm@gmail.com>
2021-06-24 13:04:56 -0500

    Se agrego las herramientas necesarias para la interfaz modificar Cita

--- ProyectoAltavista/ModificarCita.Designer.cs
+++ ProyectoAltavista/ModificarCita.Designer.cs
@@ -29,12 +29,188 @@ namespace ProyectoAltavista
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModificarCita));
+            this.button1 = new System.Windows.Forms.Button();
+            this.btDeshabilitar = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.checkBoxRealizada = new System.Windows.Forms.CheckBox();
+            this.checkBoxHabilitado = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxHoraCita = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerCita = new System.Windows.Forms.DateTimePicker();
+            this.lbFechaCita = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Lucida Fax", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(577, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 54);
+            this.button1.TabIndex = 54;
+            this.button1.Text = "Realizada";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btDeshabilitar
+            // 
+            this.btDeshabilitar.Font = new System.Drawing.Font("Lucida Fax", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDeshabilitar.Location = new System.Drawing.Point(577, 219);
+            this.btDeshabilitar.Name = "btDeshabilitar";
+            this.btDeshabilitar.Size = new System.Drawing.Size(188, 54);
+            this.btDeshabilitar.TabIndex = 53;
+            this.btDeshabilitar.Text = "Deshabilitar";
+            this.btDeshabilitar.UseVisualStyleBackColor = true;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Font = new System.Drawing.Font("Lucida Fax", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Location = new System.Drawing.Point(577, 378);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(188, 54);
+            this.btnRegresar.TabIndex = 52;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Lucida Fax", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(577, 135);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(188, 54);
+            this.btnModificar.TabIndex = 51;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRealizada
+            // 
+            this.checkBoxRealizada.AutoSize = true;
+            this.checkBoxRealizada.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxRealizada.Location = new System.Drawing.Point(211, 387);
+            this.checkBoxRealizada.Name = "checkBoxRealizada";
+            this.checkBoxRealizada.Size = new System.Drawing.Size(167, 26);
+            this.checkBoxRealizada.TabIndex = 50;
+            this.checkBoxRealizada.Text = "Cita Realizada";
+            this.checkBoxRealizada.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxHabilitado
+            // 
+            this.checkBoxHabilitado.AutoSize = true;
+            this.checkBoxHabilitado.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxHabilitado.Location = new System.Drawing.Point(16, 387);
+            this.checkBoxHabilitado.Name = "checkBoxHabilitado";
+            this.checkBoxHabilitado.Size = new System.Drawing.Size(129, 26);
+            this.checkBoxHabilitado.TabIndex = 49;
+            this.checkBoxHabilitado.Text = "Habilitado";
+            this.checkBoxHabilitado.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 57);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Modificar Cita";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 24);
+            this.label3.TabIndex = 46;
+            this.label3.Text = "Hora de la Cita";
+            // 
+            // comboBoxHoraCita
+            // 
+            this.comboBoxHoraCita.FormattingEnabled = true;
+            this.comboBoxHoraCita.Items.AddRange(new object[] {
+            "8:00",
+            "9:00",
+            "10:00",
+            "11:00",
+            "12:00",
+            "13:00",
+            "14:00",
+            "15:00",
+            "16:00"});
+            this.comboBoxHoraCita.Location = new System.Drawing.Point(211, 252);
+            this.comboBoxHoraCita.Name = "comboBoxHoraCita";
+            this.comboBoxHoraCita.Size = new System.Drawing.Size(167, 21);
+            this.comboBoxHoraCita.TabIndex = 45;
+            // 
+            // dateTimePickerCita
+            // 
+            this.dateTimePickerCita.Location = new System.Drawing.Point(211, 144);
+            this.dateTimePickerCita.Name = "dateTimePickerCita";
+            this.dateTimePickerCita.Size = new System.Drawing.Size(229, 20);
+            this.dateTimePickerCita.TabIndex = 44;
+            // 
+            // lbFechaCita
+            // 
+            this.lbFechaCita.AutoSize = true;
+            this.lbFechaCita.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFechaCita.Location = new System.Drawing.Point(12, 140);
+            this.lbFechaCita.Name = "lbFechaCita";
+            this.lbFechaCita.Size = new System.Drawing.Size(179, 24);
+            this.lbFechaCita.TabIndex = 43;
+            this.lbFechaCita.Text = "Fecha de la Cita";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 48;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ModificarCita
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btDeshabilitar);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.checkBoxRealizada);
+            this.Controls.Add(this.checkBoxHabilitado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxHoraCita);
+            this.Controls.Add(this.dateTimePickerCita);
+            this.Controls.Add(this.lbFechaCita);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ModificarCita";
             this.Text = "ModificarCita";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btDeshabilitar;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.CheckBox checkBoxRealizada;
+        private System.Windows.Forms.CheckBox checkBoxHabilitado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxHoraCita;
+        private System.Windows.Forms.DateTimePicker dateTimePickerCita;
+        private System.Windows.Forms.Label lbFechaCita;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
