f689d9a1f88f692fc21bfccc3bd1ef5bf8df77ef
Antonio_Keyxer <antonioguanilogil@gmail.com>
2021-06-25 11:53:49 -0500

    log sitioInmueble codigo

--- CapaLogica/logSitioInmueble.cs
+++ CapaLogica/logSitioInmueble.cs
@@ -5,10 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using CapaEntidad;
 using CapaDatos;
+using System.Data;
 
 namespace CapaLogica
 {
     public class logSitioInmueble
     {
+        #region singleton
+        private static readonly logSitioInmueble _instancia = new logSitioInmueble();
+        public static logSitioInmueble Instancia
+        {
+            get { return logSitioInmueble._instancia; }
+        }
+        #endregion singleton
+        public void RegistrarSitioInmueble(entSitioInmueble sitioInmueble)
+        {
+            datSitioInmueble.Instancia.RegistrarSitioInmueble(sitioInmueble);
+        }
+        public List<entSitioInmueble> ListarSitiosInmuebles()
+        {
+            return datSitioInmueble.Instancia.ListarSitioInmueble();
+        }
     }
 }
